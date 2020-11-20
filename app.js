647d6ee13729999bb6bbf9c1f3e50de46b5703a2
hriddhi <35338363+hriddhi@users.noreply.github.com>
2020-11-20 21:10:52 +0530

    Redirect URI Updated

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/user', usersRouter);
 app.use('/auth', authRouter);
 app.use('/anime', animeRouter);
 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,91 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/completed', (req, res, next) => {
+
+    axios.get(`https://api.myanimelist.net/v2/users/@me/animelist?status=completed&sort=list_updated_at&limit=1000&fields=my_list_status,num_episodes`, {
+        headers: {
+            'Authorization': 'Bearer ' + req.query.token
+        }
+    })
+    .then(response => {
+        console.log(response.data)
+        res.statusCode = 200
+        res.json(response.data)
+    })
+    .catch(err => {
+        next(err)
+    })
+})
+
+router.get('/plan', (req, res, next) => {
+
+    axios.get(`https://api.myanimelist.net/v2/users/@me/animelist?status=plan_to_watch&sort=list_updated_at&limit=1000&fields=my_list_status,num_episodes`, {
+        headers: {
+            'Authorization': 'Bearer ' + req.query.token
+        }
+    })
+    .then(response => {
+        console.log(response.data)
+        res.statusCode = 200
+        res.json(response.data)
+    })
+    .catch(err => {
+        next(err)
+    })
+})
+
+router.get('/watching', (req, res, next) => {
+
+    axios.get(`https://api.myanimelist.net/v2/users/@me/animelist?status=watching&sort=list_updated_at&limit=1000&fields=my_list_status,num_episodes`, {
+        headers: {
+            'Authorization': 'Bearer ' + req.query.token
+        }
+    })
+    .then(response => {
+        console.log(response.data)
+        res.statusCode = 200
+        res.json(response.data)
+    })
+    .catch(err => {
+        next(err)
+    })
+})
+
+router.get('/hold', (req, res, next) => {
+
+    axios.get(`https://api.myanimelist.net/v2/users/@me/animelist?status=on_hold&sort=list_updated_at&limit=1000&fields=my_list_status,num_episodes`, {
+        headers: {
+            'Authorization': 'Bearer ' + req.query.token
+        }
+    })
+    .then(response => {
+        console.log(response.data)
+        res.statusCode = 200
+        res.json(response.data)
+    })
+    .catch(err => {
+        next(err)
+    })
+})
+
+router.get('/dropped', (req, res, next) => {
+
+    axios.get(`https://api.myanimelist.net/v2/users/@me/animelist?status=dropped&sort=list_updated_at&limit=1000&fields=my_list_status,num_episodes`, {
+        headers: {
+            'Authorization': 'Bearer ' + req.query.token
+        }
+    })
+    .then(response => {
+        console.log(response.data)
+        res.statusCode = 200
+        res.json(response.data)
+    })
+    .catch(err => {
+        next(err)
+    })
+})
+
 
 module.exports = router;
